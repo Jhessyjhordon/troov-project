a5b7d15c726892f604e76fac97000a5415979c15
Jhessyjhordon <jhessyismael@gmail.com>
2024-12-25 11:58:13 +0100

    Ajout de la page Swagger dans le frontend via iframe

---
+++ frontend/pages/swagger.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Swagger Documentation</h1>
+        <iframe src="http://localhost:5000/api-docs" width="100%" height="800px" style="border: none;"></iframe>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SwaggerDocs",
+    };
+</script>
